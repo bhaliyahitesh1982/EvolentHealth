0e96bcd543284cfec6eb0745c83d5c7216714cd6
bhaliyahitesh1982 <53378082+bhaliyahitesh1982@users.noreply.github.com>
2021-06-06 11:36:53 +0530

    Evolent Health Web Api
    This project contain the WEB API which need to consume in any WEB application or in Mobile application. Currently I have tested the functionality in POSTMAN tool.
    It also have some code of Authentication which we can use Authentication Token to Authenticate user.

---
+++ Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using System.Web;
+
+
+namespace EvolentHealth
+{
+    public class Startup
+    {
+        /// <summary>
+        /// It will creates an OWIN pipeline for hosting Web API, it  also configures the routing
+        /// </summary>
+        /// <param name="app"></param>
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //Need register Filter here to exeute based on Attribute set on the Controller or class
+            config.Filters.Add(new BasicAuthenticationAttribute());
+
+            //Set the response type in the for of JASON
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
+            app.UseWebApi(config);
+        }
+    }
+}
